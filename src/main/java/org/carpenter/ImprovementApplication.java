01b96b0d8f353f67fe330ab16feebcea28bda7f1
Jeongyong Joo <jeongyong95@gmail.com>
2021-01-01 18:54:57 +0900

    Improvement. 훌륭한 나무를 만들어보자.

---
+++ src/main/java/org/carpenter/ImprovementApplication.java
@@ -0,0 +1,13 @@
+package org.carpenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImprovementApplication.class, args);
+    }
+
+}
